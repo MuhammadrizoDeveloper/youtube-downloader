537408cd022d0ae5be83f1753b623142295bf3dd
MuhammadrizoDeveloper <yusufovmuhammadrizodev@gmail.com>
2021-08-27 09:18:48 +0500

    first commit

---
+++ code.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const app = express();
+const { host, PORT } = require('./config.js'); 
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.get("/",function(request,response){
+	response.sendFile(__dirname + "public/index.html");
+});
+
+app.get("/videoInfo",async function(request,response){
+	const videoURL = request.query.videoURL;
+	const info = await ytdl.getInfo(videoURL);
+	response.status(200).json(info);
+});
+
+app.get("/download",function(request,response){
+	const videoURL = request.query.videoURL;
+	const itag = request.query.itag;
+	const format = request.query.format;
+	response.header("Content-Disposition",'attachment;\ filename="video.'+format+'"');
+	ytdl(videoURL,{
+		filter: format => format.itag == itag
+	}).pipe(response);
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://${host}:${PORT}`));
---
+++ config.js
@@ -0,0 +1,7 @@
+const host = 'localhost'
+const PORT = process.env.PORT || 5000
+
+module.exports = {
+    host,
+    PORT
+}
